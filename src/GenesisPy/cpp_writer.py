94ebcad99a6c1c42cd720e03d52991d9f0c20daf
mattagape <40172038+mattagape@users.noreply.github.com>
2026-05-07 13:25:21 +0100

    Adding class to generate C++ code.

---
+++ src/GenesisPy/cpp_writer.py
@@ -0,0 +1,18 @@
+# Class to handle genration of C++ code.
+
+class CppWriter:
+
+    def generate_header(self):
+        '''Generate "myclasses.h" file.'''
+        pass
+
+
+    def  generate_impl(self):
+        '''Generate"myclasses.cpp" file.'''
+        pass
+
+
+    def generate_main(self):
+        '''Generate simulation.cpp file.'''
+        pass
+
---
+++ tests/test_cpp_writer.py
@@ -0,0 +1,15 @@
+# Tests for CppWriter class.
+
+import pytest
+from GenesisPy import cpp_writer
+
+def test_generate_header():
+    pass
+
+
+def test_generate_impl():
+    pass
+
+
+def test_generate_main():
+    pass
